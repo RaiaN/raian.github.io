658af86f5bfb59a0f72b3275d4bbe7fe8bebac60
Peter Leontev <raianknight@gmail.com>
2019-04-16 21:25:19 +0300

    Updates

--- src/components/external_link.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default props => 
-(
-    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <a href={props.url}>{props.url_name}</a>
-    </li>
-)
