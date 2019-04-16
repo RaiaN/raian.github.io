b7625cfccd3a0fe52cc77be31e5505f4faa4dd78
Peter Leontev <raianknight@gmail.com>
2019-04-16 21:27:03 +0300

    Fix about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,25 +4,24 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
       <p>My name Is Peter.</p>
-      <p>I am generalist programmer passionated about building game technologies and improving quality of pipelines.</p>
+      <p>I am generalist programmer passionated about creating game technologies and improving quality of pipelines.</p>
       <p>Besides tech I enjoy bouldering, roller skating, watching esport and playing computer games.</p>
 
-      <h4>Contacts:</h4>
+      <h4>Contact me:</h4>
       <p>
           <ul>
             <li>
-            <a href="mailto:leontev.petr@gmail.com">Email</a>
+              <a href="mailto:leontev.petr@gmail.com">Email</a>
             </li>
+
             <li>
-            <a href="https://www.linkedin.com/in/peter-leontev-67b34863/">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/peter-leontev-67b34863/">LinkedIn</a>
             </li>
+
             <li>
-            <a href="https://twitter.com/PeterLeontev">Twitter</a>
+              <a href="https://twitter.com/PeterLeontev">Twitter</a>
             </li>
-          </ul>
-          
-          
-          
+          </ul>          
       </p>
   </Layout>
 )
