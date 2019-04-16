9ed101913dbabafc97344ef9d980cbb3bf127c81
Peter Leontev <raianknight@gmail.com>
2019-04-16 14:41:43 +0300

    Add default pages

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header HeaderName="About Peter Leontev"/>
+    <p>Generalist programmer passionated about building game technologies and improving quality of DDC pipelines</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple` }}>
+        <Header HeaderName="Welcome to Peter's blog!"/>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />  
+    </div>
+)
