859ef544965db41bfa117666cc11b424c5bc7435
Peter Leontev <raianknight@gmail.com>
2019-04-16 15:24:18 +0300

    Updates

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Header from "../components/header"
-
-export default () => (
-  <div style={{ color: `teal` }}>
-    <Header HeaderName="About Peter Leontev"/>
-    <p>Generalist programmer passionated about building game technologies and improving quality of DDC pipelines</p>
-  </div>
-)
