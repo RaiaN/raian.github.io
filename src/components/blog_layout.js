0895bb8ef13b111d2ec240d3a2b9522344a4f5b9
Peter Leontev <raianknight@gmail.com>
2019-04-18 00:06:20 +0300

    fix blog layout css

--- src/components/blog_layout.js
+++ src/components/blog_layout.js
@@ -7,14 +7,7 @@ class BlogLayout extends React.Component {
     const { children } = this.props    
 
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
