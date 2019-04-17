918b931485f828cb82f87fe3aeab3d1271b6020f
Peter Leontev <raianknight@gmail.com>
2019-04-17 22:05:15 +0300

    ~ Setup blog template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,66 @@
 import React from "react"
-import Layout from "../components/layout"
+import { Link, graphql } from "gatsby"
 
-export default () => (
-  <Layout>
-      <p>
-          There will be blog!
-      </p>
+import Bio from "../components/bio"
+import BlogLayout from "../components/blog_layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
-  </Layout>
+export default props =>
+(
+  <BlogLayout location={props.location} title={props.data.site.siteMetadata.title}>
+    <SEO
+      title="All posts"
+      keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+    />
+    <Bio />
+    {props.data.allMarkdownRemark.edges.map(({ node }) => {
+      const title = node.frontmatter.title || node.fields.slug
+      return (
+        <div key={node.fields.slug}>
+          <h3
+            style={{
+              marginBottom: rhythm(1 / 4),
+            }}
+          >
+            <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+              {title}
+            </Link>
+          </h3>
+          <small>{node.frontmatter.date}</small>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: node.frontmatter.description || node.excerpt,
+            }}
+          />
+        </div>
+      )
+    })}
+  </BlogLayout>
 )
+
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
