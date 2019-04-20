4170d32ef8cf3e11bfe3682da73cf57782b4b14c
Peter Leontev <raianknight@gmail.com>
2019-04-20 19:45:52 +0300

    Add global css, fix index page text

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,22 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import ListLink from "./listLink"
 import ExternalLink from "./externalLink"
+import "../global.css"
 
 
 
 class Layout extends React.Component {
     render() {
-        console.log(this.props.children);
         return (
             <div style={{ margin: `3rem auto`, maxWidth: 800}}>
                 <header style={{ marginBottom: `1.5rem` }}>
                     <Link to="/" style={{ backgroundImage: `none` }}>
                         <h3 style={{ display: 'inline'}}>Peter Leontev </h3>
                     </Link>
-                    
                     <ul style={{ listStyle: `none`, float: `right` }}>
                         <ListLink to="/">About</ListLink>
-                        <ListLink to="/projects/">Projects</ListLink>
+                        <ListLink to="/projects/">Work</ListLink>
                         <ListLink to="/blog/">Blog</ListLink>
                         <ExternalLink url="http://google.ru" url_name="CV"/>
                     </ul>
