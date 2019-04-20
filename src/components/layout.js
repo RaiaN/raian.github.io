bacc0eb3dc0613dfd7d72ee1e3ee378fa88074fb
Peter Leontev <raianknight@gmail.com>
2019-04-20 12:35:10 +0300

    Set up projects

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,22 +4,22 @@ import ListLink from "../components/list_link"
 import ExternalLink from "../components/external_link"
 
 
-
 export default ({ children }) => 
 (
     <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
-        <header style={{ marginBottom: `1.5rem` }}>
-            <Link to="/" style={{ backgroundImage: `none` }}>
-                <h3 style={{ display: 'inline'}}>Peter Leontev </h3>
-            </Link>
             
-            <ul style={{ listStyle: `none`, float: `right` }}>
-                <ListLink to="/">About</ListLink>
-                <ListLink to="/projects/">Projects</ListLink>
-                <ListLink to="/blog/">Blog</ListLink>
-                <ExternalLink url="http://google.ru" url_name="CV"/>
-            </ul>
-        </header>
+            <header style={{ marginBottom: `1.5rem` }}>
+                <Link to="/" style={{ backgroundImage: `none` }}>
+                    <h3 style={{ display: 'inline'}}>Peter Leontev </h3>
+                </Link>
+                
+                <ul style={{ listStyle: `none`, float: `right` }}>
+                    <ListLink to="/">About</ListLink>
+                    <ListLink to="/projects/">Projects</ListLink>
+                    <ListLink to="/blog/">Blog</ListLink>
+                    <ExternalLink url="http://google.ru" url_name="CV"/>
+                </ul>
+            </header>
         {children}
     </div>
 )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,60 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from 'gatsby'
+import PaginatedPageTemplate from "../templates/paginatedPageTemplate"
 
+const path = require(`path`)
 
 class ProjectsComponent extends React.Component {
-
   render() {
+    const projectsInfo = this.props.data.images.edges.map(edge => {
+      return {
+          "large": edge.node.childImageSharp.fluid.originalImg,
+          "small": edge.node.childImageSharp.fixed.src
+      }
+    })
+
+    const Projects = projectsInfo.map(projectInfo => <PaginatedPageTemplate key={projectInfo.node.id} post={projectInfo.node} />)
 
     return (
       <Layout>
-          
+          {Projects} 
       </Layout>
-    );
+    )
   }
-
 }
 
 export default ProjectsComponent;
+
+export const projectsComponentQuery = graphql`
+  query 
+  {
+    images: allFile
+    (
+        filter: 
+        {
+          extension: {regex: "/(jpeg|jpg|png)/"},
+          sourceInstanceName: {eq: "images"}
+        }
+    ) 
+    {
+      edges 
+      {
+        node 
+        {
+          childImageSharp
+          {
+            fixed(quality: 95, width: 300, height: 300) 
+            {
+              src
+            }
+            fluid 
+            {
+              originalImg
+            }
+          }
+        }
+      }
+    }
+  } 
+`
