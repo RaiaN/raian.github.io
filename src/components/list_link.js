1e45cb67f7ef973563515b4d6ad055dfc44258b9
Peter Leontev <raianknight@gmail.com>
2019-04-20 13:11:52 +0300

    Remove/rename components

--- src/components/list_link.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Link } from "gatsby"
-
-export default props => 
-(
-    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <Link to={props.to}>{props.children}</Link> 
-    </li>
-)
