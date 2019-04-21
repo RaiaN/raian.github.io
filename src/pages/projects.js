8309375ae57ec53c3b8ba7b61bbfb18a5851eb2a
Peter Leontev <raianknight@gmail.com>
2019-04-21 14:00:15 +0300

    Fix naming

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,17 +2,17 @@ import React from "react"
 import Layout from "../components/layout"
 import ProjectInfo from "../components/projectInfo"
 
-import project_1_image from "../../content/images/1.jpeg"
-import project_2_image from "../../content/images/2.jpeg"
+import project_1_image_url from "../../content/images/1.jpeg"
+import project_2_image_url from "../../content/images/2.jpeg"
 
 
 class ProjectsComponent extends React.Component {
   render() {
     return (
         <Layout>
-            <ProjectInfo image_url={project_1_image} description="Project 1 name and description 1"/>
-            <ProjectInfo image_url={project_2_image} description="Project 2 name and description 2 maybe"/>
-            <ProjectInfo image_url={project_2_image} description="Project 3 and no description"/>
+            <ProjectInfo image_url={project_1_image_url} description="Project 1 name and description 1"/>
+            <ProjectInfo image_url={project_2_image_url} description="Project 2 name and description 2 maybe"/>
+            <ProjectInfo image_url={project_1_image_url} description="Project 3 and no description"/>
         </Layout>
     )
   }
