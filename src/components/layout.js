39f7ba0bb82b0985a91222a31051aa340975f01a
Peter Leontev <raianknight@gmail.com>
2019-04-21 13:33:20 +0300

    Fix project info and style. Add noise background

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import ListLink from "./listLink"
 import ExternalLink from "./externalLink"
-import "../global.css"
+import "./global.css"
 
 
 
 class Layout extends React.Component {
     render() {
         return (
-            <div style={{ margin: `3rem auto`, maxWidth: 800}}>
+            <div style={{ margin: `3rem auto`, maxWidth: 800 }}>
                 <header style={{ marginBottom: `1.5rem` }}>
                     <Link to="/" style={{ backgroundImage: `none` }}>
                         <h3 style={{ display: 'inline'}}>Peter Leontev </h3>
                     </Link>
                     <ul style={{ listStyle: `none`, float: `right` }}>
                         <ListLink to="/"><b>About</b></ListLink>
-                        <ListLink to="/work/"><b>Work</b></ListLink>
+                        <ListLink to="/projects/"><b>Projects</b></ListLink>
                         <ListLink to="/blog/"><b>Blog</b></ListLink>
                         <ExternalLink url="http://google.ru" url_name="CV"/>
                     </ul>
--- src/components/projectInfo.js
+++ src/components/projectInfo.js
@@ -5,7 +5,9 @@ const ProjectInfo = props => {
     return (
         <React.Fragment>
             <div className="img-container">
-                <img src={props.item.s} alt="" title=""></img>
+                <Link to="/blog/">
+                    <img src={props.item.s} alt="" title=""></img>
+                </Link>
                 <p>
                     Project name and description
                 </p>
@@ -16,14 +18,13 @@ const ProjectInfo = props => {
                     display: inline;
                     float: left;
                     width: 800px;
-                    border-top: 2px dashed;
-                    border-left: 2px dashed;
-                    border-right: 2px dashed;
-                    padding: 1px;
-                    border-size: 0.5px;
-                    margin-bottom: -27px;
-                    position: relative;
-                    top: 5%;
+                    border-top: 2px dotted;
+                    border-left: 2px dotted;
+                    border-right: 2px dotted;
+                    border-bottom: 2px dotted;
+                    padding: 0px;
+                    margin-top: 0px;
+                    margin-bottom: 1px;
                 }
 
                 .img-container img {
@@ -32,12 +33,22 @@ const ProjectInfo = props => {
                     transition: 0.2s ease-in-out;
                     z-index: 2;
                     float: left;
-                    margin-right: 16px;
+                    margin-right: 10px;
+                    margin-left: 4px;
+                    margin-top: 4px;
+                    margin-bottom: 4px;
 
                     :hover {
                         opacity: 0.95;
                         transform: scale(1.01);
                     }
+                }
+
+                .img-container p {
+                    margin-right: 0px;
+                    margin-left: 8px;
+                    margin-top: 4px;
+                    margin-bottom: 4px;
                 }
             `}
             </style>
