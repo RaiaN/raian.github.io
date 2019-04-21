e06a1856756d9e953ff5299d7ef6c961457035b1
Peter Leontev <raianknight@gmail.com>
2019-04-21 15:34:40 +0300

    Fix css for mobile devices

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,20 +9,78 @@ import "./global.css"
 class Layout extends React.Component {
     render() {
         return (
-            <div style={{ margin: `3rem auto`, maxWidth: 800 }}>
-                <header style={{ marginBottom: `1.5rem` }}>
-                    <Link to="/" style={{ backgroundImage: `none` }}>
-                        <h3 style={{ display: 'inline'}}>Peter Leontev </h3>
-                    </Link>
-                    <ul style={{ listStyle: `none`, float: `right` }}>
-                        <ListLink to="/"><b>About</b></ListLink>
-                        <ListLink to="/projects/"><b>Projects</b></ListLink>
-                        <ListLink to="/blog/"><b>Blog</b></ListLink>
-                        <ExternalLink url="http://google.ru" url_name="CV"/>
-                    </ul>
-                </header>
-                {this.props.children}
-            </div>
+            <React.Fragment>
+                <div className="layout-container">
+                    <div className="layout-header">
+                        <Link to="/">
+                            <h3 className="author">Peter Leontev </h3>
+                        </Link>
+                        <ul className="navigation">
+                            <ListLink to="/"><b>About</b></ListLink>
+                            <ListLink to="/projects/"><b>Projects</b></ListLink>
+                            <ListLink to="/blog/"><b>Blog</b></ListLink>
+                            <ExternalLink url="http://google.ru" url_name="CV"/>
+                        </ul>
+                    </div>
+                    <div className="layout-content">
+                        {this.props.children}
+                    </div>
+                </div>
+                <style jsx>
+                {`
+                    .layout-container
+                    {
+                        margin: 3rem auto;
+                        max-width: 800px;
+                        padding: 1rem;
+                    }
+
+                    .layout-header
+                    {   
+                        margin-left: 0px;
+                        margin-bottom: 16vm;
+                        display: inline;
+                    }
+
+                    .author 
+                    {
+                        display: inline;
+                    }
+
+                    .navigation
+                    {
+                        float: right;
+                        margin-bottom: 0px;
+                    }
+
+                    .layout-content
+                    {
+                        margin-top: 2em;
+                    }
+
+                    @media (max-width: 500px) 
+                    {
+                        .author 
+                        {
+                            margin-top: 0px;
+                            margin-bottom: 1px;
+                            display: block;
+                        }
+
+                        .navigation
+                        {
+                            margin-left: 0px;
+                            float: none;
+                        }
+
+                        .layout-content
+                        {
+                            margin-top: 0.5em;
+                        }
+                    }
+                `}
+                </style>
+            </React.Fragment>
         )
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,23 @@ import { rhythm } from "../utils/typography"
 
 export default () => (
     <Layout>
-        <p>My name is Peter.</p>
-        
         <p>
             I am generalist programmer passionated about creating game technologies and improving quality of pipelines. 
             These days I mostly work with Unreal Engine, C++ and Python.
         </p>
-
         <p>
             You can find out more about me 
             through my <Link style={{color: `#b25b05`}} to="/projects/">projects experience</Link> and <Link style={{color: `#b25b05`}} to="/blog/">technical blog</Link>.
         </p>
-
         <p>Besides tech I enjoy bouldering, roller skating, watching esport and playing computer games.</p>
-
-        <h4>Contact me:</h4>
+        <b>Contact me:</b>
         <ul style={{ marginLeft: rhythm(1 / 2) }}>
             <li>
                 <a style={{color: '#b25b05' }} href="mailto:leontev.petr@gmail.com">Email</a>
             </li>
-
             <li>
                 <a style={{color: '#b25b05' }} href="https://www.linkedin.com/in/peter-leontev-67b34863/">LinkedIn</a>
             </li>
-
             <li>
                 <a style={{color: '#b25b05' }} href="https://twitter.com/PeterLeontev">Twitter</a>
             </li>
