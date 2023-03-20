d4c2c79dfa0dd020e162d88fd6a748d46c12e704
RaiaN <raianknight@gmail.com>
2023-03-20 12:24:29 +0000

    Enable plugin, fix deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-styled-jsx",
       options: {
-        //jsxPlugins: ["styled-jsx-plugin-postcss"],
+        jsxPlugins: ["styled-jsx-plugin-postcss"],
       },
     },
     {
